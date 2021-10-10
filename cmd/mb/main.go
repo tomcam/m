4fcfc93182a0b75c2c96c00f17721a2ca4741d80
Tom Campbell <tomcampbell@gmail.com>
2021-10-10 15:31:00 -0700

    Refactor to move code out of main

--- cmd/mb/main.go
+++ cmd/mb/main.go
@@ -1,105 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"github.com/tomcam/m/pkg/app"
-	"github.com/tomcam/m/pkg/mdext"
-	"github.com/tomcam/m/pkg/util"
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/parser"
-	"github.com/yuin/goldmark/renderer"
-	//highlighting "github.com/yuin/goldmark-highlighting"
-	"github.com/yuin/goldmark/extension"
-	"github.com/yuin/goldmark/renderer/html"
-	"github.com/yuin/goldmark/text"
 	"io"
 	"os"
 )
-
-func mdToHTML(source []byte) []byte {
-	// Get a parser object.
-	ctx := parser.NewContext()
-	p := newParser().Parser()
-	node := p.Parse(text.NewReader(source), parser.WithContext(ctx))
-	// Create variable-sized buffer for parsed output.
-	buf := new(bytes.Buffer)
-	// Convert the Markdown file to HTML.
-	if err := newParser().Renderer().Render(buf, source, node); err != nil {
-		// TC: update error handling
-		// a.QuitError(errs.ErrCode("0920", err.Error()))
-		panic("Parse error")
-		// TODO: Hmmm... this should probably return an error
-		return nil
-	}
-	// Return the HTML.
-	fmt.Println("mdToHTML: \n" + string(buf.Bytes()))
-	return buf.Bytes()
-}
-
-// mdFileToHTML converts the markdown file in filename to HTML.
-// It may include optional front matter.
-func mdFileToHTML(filename string) []byte {
-	// Read file into a byte slice.
-	s := util.FileToBytes(filename)
-	return mdToHTML(s)
-
-	// Get a parser object.
-	ctx := parser.NewContext()
-	p := newParser().Parser()
-	node := p.Parse(text.NewReader(s), parser.WithContext(ctx))
-	// Create variable-sized buffer for parsed output.
-	buf := new(bytes.Buffer)
-	// Convert the Markdown file to HTML.
-	if err := newParser().Renderer().Render(buf, s, node); err != nil {
-		// TC: update error handling
-		// a.QuitError(errs.ErrCode("0920", err.Error()))
-		panic("Parse error")
-		// TODO: Hmmm... this should probably return an error
-		return nil
-	}
-	// Return the HTML.
-	return buf.Bytes()
-}
-
-func newParser() goldmark.Markdown {
-	exts := []goldmark.Extender{
-		//mdext.New(mdext.WithTable()), extension.Table,
-		mdext.New(),
-    extension.Table,
-		extension.GFM,
-		extension.DefinitionList,
-		extension.Footnote,
-		// TC: Add highlighting options
-		/*
-			highlighting.NewHighlighting(
-				highlighting.WithStyle(a.Site.MarkdownOptions.HighlightStyle),
-				highlighting.WithFormatOptions()),
-		*/
-
-	}
-
-	parserOpts := []parser.Option{parser.WithAttribute(), parser.WithAutoHeadingID()}
-
-	renderOpts := []renderer.Option{
-		// WithUnsafe is required for HTML templates to work properly
-		html.WithUnsafe(),
-		html.WithXHTML(),
-	}
-	// TC: Add as option?
-	/*
-		if a.Site.MarkdownOptions.hardWraps {
-			renderOpts = append(renderOpts, html.WithHardWraps())
-		}
-	*/
-
-	return goldmark.New(
-		goldmark.WithExtensions(exts...),
-		goldmark.WithParserOptions(parserOpts...),
-		goldmark.WithRendererOptions(renderOpts...),
-	)
-}
 
 func main() {
 	filename := "."
@@ -117,6 +24,7 @@ func main() {
 	default: // program name only
 		// Same as build
 	}
+	a := app.NewApp(filename)
 	fmt.Printf("\tCommand: %s\n", command)
 	fmt.Printf("Args: %v\nArg count: %v\nCommand: %s\nFilename: %s\n",
 		os.Args, len(os.Args), command, filename)
@@ -124,9 +32,8 @@ func main() {
 	case "build":
 		fmt.Println(string(mdFileToHTML(filename)))
 	case "new", "newsite":
-		a := app.NewApp(filename)
-		a.NewSite()
-		fmt.Printf("\tProject path: %s\n", a.Site.Path)
+			a.NewSite()
+		  fmt.Printf("\tProject path: %s\n", a.Site.Path)
 	}
 
 }
