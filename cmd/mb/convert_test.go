17e945013a326b16ad327c060e38465d50972f2d
Tom Campbell <tomcampbell@gmail.com>
2021-10-10 15:14:09 -0700

    Beef up test cases

--- cmd/mb/convert_test.go
+++ cmd/mb/convert_test.go
@@ -11,24 +11,35 @@ func TestConversion(t *testing.T) {
 		mdSrc string
 		want  string
 	}{
+    // Ensure front matter isn't included in output 
+    // Also ensure an ID is added to the header.
 		{`
 		   +++
        Stuff: here
        +++
        # 1
 		`, "<h1 id=\"1\">1</h1>\n"},
+
+    // Ensure whitespace in input is ignored properly
+    // (compare output with next test)
 		{`
-       hello
-		`, "<p>hello</p>\n"},
+       hello, world.
+		`, "<p>hello, world.</p>\n"},
+
+    // Ensure whitespace in input is consistent
+    // with previous test.
 		{`
-       hello
+       hello, world.
 
 
-		`, "<p>hello</p>\n"},
+		`, "<p>hello, world.</p>\n"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.mdSrc, func(t *testing.T) {
-      //got := mdToHTML(tt.mdSrc)
+      // Go through each test case.
+      // Dedent() removes semantically unnecessary
+      // whitespace from output so it can be reliably
+      // compared to the expected output.
       got := string(mdToHTML([]byte(texts.Dedent(tt.mdSrc))))
 			if diff := cmp.Diff(tt.want, got); diff != "" {
 				t.Errorf(" mismatch (-want +got):\n%s", diff)
