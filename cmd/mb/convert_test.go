05aa5d7d43f751e6ceadb247a91a3a7891520d50
Tom Campbell <tomcampbell@gmail.com>
2021-10-10 09:17:44 -0700

    Begin realistic testing. Stop auto generating YAML data as table.

--- cmd/mb/convert_test.go
+++ cmd/mb/convert_test.go
@@ -1,17 +1,53 @@
 package main
 
 import (
-  "testing"
+	"github.com/google/go-cmp/cmp"
+	"github.com/tomcam/m/pkg/texts"
+	//"github.com/yuin/goldmark/text"
+	"testing"
 )
 
 var test1 = `# 1`
 
 // Very beginnings of end-to-end testing
+func iTestConversion(t *testing.T) {
+	got := string(mdToHTML([]byte(test1)))
+	want := "<h1 id=\"1\">1</h1>\n"
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+
+
 func TestConversion(t *testing.T) {
-  got := string(mdToHTML([]byte(test1)))
-  want := "<h1 id=\"1\">1</h1>\n"
-  if got != want {
-          t.Errorf("got %q, wanted %q", got, want)
-  }
+	tests := []struct {
+		mdSrc string
+		want  string
+	}{
+		{texts.Dedent(`
+		   +++
+       Stuff: here
+       +++
+       # 1
+		`), "<h1 id=\"1\">1</h1>\n"},
+		{texts.Dedent(`
+       hello
+		`), "<p>hello</p>\n"},
+		{texts.Dedent(`
+       hello
+
+
+		`), "<p>hello</p>\n"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.mdSrc, func(t *testing.T) {
+      //got := mdToHTML(tt.mdSrc)
+      got := string(mdToHTML([]byte(tt.mdSrc)))
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf(" mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
 }
 
